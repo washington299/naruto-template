8184a9f99c5166ac4a8d77384e109be7b4ad87b7
Washington Campos <washingtoncampos9@gmail.com>
2019-09-18 01:37:51 -0300

    top done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
+import BannerImage from './components/BannerImage';
 
 function App() {
   return (
     <>
       <Header />
+      <BannerImage />
     </>
   );
 }
---
+++ src/components/BannerImage/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const BannerImage = () => {
+    return(
+        <div className="banner_image" id="anime_area"></div>
+    );
+};
+
+export default BannerImage;
