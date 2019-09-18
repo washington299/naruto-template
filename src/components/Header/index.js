4ce41d25150b408dbfa46e3e3229708d9f4bf18b
Washington Campos <washingtoncampos9@gmail.com>
2019-09-18 00:53:18 -0300

    Header done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import './style.css';
+import menuMobile from '../../images/menu_mobile.png';
 
 const Header = () => {
+
+    const [menuOpened, setMenuOpened] = useState(false);
+
+    const openMenu = () => {
+        setMenuOpened(!menuOpened);
+    };
+
     return(
         <header>
-            <h1>Hello world</h1>
+            <div className="container">
+                <div className="logo" id="start"><a>Naruto Uzumaki</a></div>
+                <div className="menu">
+                    <nav>
+                        <div className="menuMobile" onClick={openMenu}>
+                            <img src={menuMobile} title="menu-mobile" width="50" height="50" />
+                        </div>
+                        <ul className={menuOpened ? 'opened' : ''}>
+                            <a><li>Anime</li></a>
+                            <a><li>About</li></a>
+                            <a><li>Characters</li></a>
+                            <a><li>Jutsus</li></a>
+                            <a><li>Games</li></a>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </header>
     );
 };
