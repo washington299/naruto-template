af5adfff0ac18c7c3e9c6feddfe32186ce058955
Washington Campos <washingtoncampos9@gmail.com>
2019-09-19 02:08:47 -0300

    Updates

---
+++ precache-manifest.a8956800a0361d280f46449450c77ba1.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb1484e958dd7027d3b7d049b9e415f",
+    "url": "/naruto-template/index.html"
+  },
+  {
+    "revision": "1322aa1a4fda56c68b3b",
+    "url": "/naruto-template/static/css/main.94d88d87.chunk.css"
+  },
+  {
+    "revision": "822bd91a4c43637dd7ca",
+    "url": "/naruto-template/static/js/2.d7cefbef.chunk.js"
+  },
+  {
+    "revision": "1322aa1a4fda56c68b3b",
+    "url": "/naruto-template/static/js/main.0d7db747.chunk.js"
+  },
+  {
+    "revision": "748edd0065cb41c382cb",
+    "url": "/naruto-template/static/js/runtime~main.2a300476.js"
+  },
+  {
+    "revision": "bf022607d01687248905602d9c194f76",
+    "url": "/naruto-template/static/media/game1.bf022607.jpg"
+  },
+  {
+    "revision": "192c5d07559206fbfacfc25bb9d95447",
+    "url": "/naruto-template/static/media/game10.192c5d07.jpg"
+  },
+  {
+    "revision": "d7b512f1841085f519f211a51c33c765",
+    "url": "/naruto-template/static/media/game11.d7b512f1.jpg"
+  },
+  {
+    "revision": "4619e5f0cb43bc23332055af3640428a",
+    "url": "/naruto-template/static/media/game12.4619e5f0.png"
+  },
+  {
+    "revision": "271ce737809eda31b56f3bd70e0f3692",
+    "url": "/naruto-template/static/media/game13.271ce737.jpg"
+  },
+  {
+    "revision": "861bdfe45504d225d7d9b5b386d3cf38",
+    "url": "/naruto-template/static/media/game14.861bdfe4.png"
+  },
+  {
+    "revision": "9deaacb23b255a3e999521eb167d2696",
+    "url": "/naruto-template/static/media/game2.9deaacb2.jpg"
+  },
+  {
+    "revision": "000e2686093b00fb338669355daaf166",
+    "url": "/naruto-template/static/media/game3.000e2686.jpg"
+  },
+  {
+    "revision": "2c62d077d908ebd46acd2d2fd908a889",
+    "url": "/naruto-template/static/media/game4.2c62d077.jpg"
+  },
+  {
+    "revision": "f4152cbcb47119f8ff4d918737c985f1",
+    "url": "/naruto-template/static/media/game5.f4152cbc.jpg"
+  },
+  {
+    "revision": "cc6997869ab692fd52c2fc65b61126f5",
+    "url": "/naruto-template/static/media/game6.cc699786.jpg"
+  },
+  {
+    "revision": "e9eef9401b8e8ce79a4660d4a2471c00",
+    "url": "/naruto-template/static/media/game7.e9eef940.jpg"
+  },
+  {
+    "revision": "8b051966d7b0037c07c13f65d1e9abc4",
+    "url": "/naruto-template/static/media/game8.8b051966.jpg"
+  },
+  {
+    "revision": "224849b748a58809ce3167709dbbfca2",
+    "url": "/naruto-template/static/media/game9.224849b7.jpg"
+  },
+  {
+    "revision": "82097f6a942eaddb730bbf80a087bb5d",
+    "url": "/naruto-template/static/media/genjutsu.82097f6a.png"
+  },
+  {
+    "revision": "a51e869418bdf4d705d27ee8653e736d",
+    "url": "/naruto-template/static/media/kakashi.a51e8694.png"
+  },
+  {
+    "revision": "bcddbead2fdbc1138144e8b25f40a05b",
+    "url": "/naruto-template/static/media/menu_mobile.bcddbead.png"
+  },
+  {
+    "revision": "8ca1151cf54ae5e2bfd7d875090f13be",
+    "url": "/naruto-template/static/media/naruto.8ca1151c.png"
+  },
+  {
+    "revision": "e34eb28bcaedbe9ef65b622f6030626c",
+    "url": "/naruto-template/static/media/naruto_banner.e34eb28b.jpg"
+  },
+  {
+    "revision": "4cdef522ba6303801e5c66921c86a1ef",
+    "url": "/naruto-template/static/media/ninjutsu.4cdef522.jpg"
+  },
+  {
+    "revision": "f22f002f403ab055ea1a241db846ca8c",
+    "url": "/naruto-template/static/media/sakura.f22f002f.png"
+  },
+  {
+    "revision": "7f36ae99874260a9da5b8bbe1851e425",
+    "url": "/naruto-template/static/media/sasuke.7f36ae99.png"
+  },
+  {
+    "revision": "322421920d3539633f2576ed4b5567d6",
+    "url": "/naruto-template/static/media/taijutsu.32242192.png"
+  }
+]);
